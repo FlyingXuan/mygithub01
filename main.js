0eb5d7145dc99bb3a6c52bc91afa99f1b7894900
xuan的git <773180822@qq.com>
2018-08-21 22:42:29 +0800

    原来这个非写不可

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("Hello World!");
+console.log(1);
+console.log(1.2);
+console.log(true);
+console.log(false);
+
+var a = 3;
+console.log(3);
+
+/*var list_table = {
+    0:3,
+    hell0:true,
+    liuxuan:19,
+}
+console.log(list_table);
+console.log(list_table.hell0);
+console.log(list_table["liuxuan"]);
+list_table["liumi"] = 16
+console.log(list_table.liumi);*/
+var fun1 = function(param1,param2){
+    console.log("Hello,World!",param1,param2);
+}
